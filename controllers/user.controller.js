6a661df041dd4f7c7f884a6bfa6006c48415c16d
RiddleASL <n00212142@iadt.ie>
2025-01-05 20:48:34 +0000

    extra validation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,11 +3,30 @@ const User = require('../models/user.model');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const validateEmail = (email) =>{
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+}
+
 const register = (req, res) => {
     let newUser = new User(req.body);
     newUser.password = bcrypt.hashSync(req.body.password, 10);
-
     console.log(newUser);
+
+    //validate user input
+    if (!newUser.email || !newUser.username || !newUser.password) {
+        return res.status(400).json({
+            msg: 'You need to send email, username and password'
+        });
+    } else if (newUser.password.length < 6) {
+        return res.status(400).json({
+            msg: 'Password should be at least 6 characters long'
+        });
+    } else if (!validateEmail(newUser.email)) {
+        return res.status(400).json({
+            msg: 'Invalid email address'
+        });
+    }
 
     newUser.save((err, user) => {
         if (err) {
